a900cffa5020a5b10864b14dc0ed08834ab251b1
prajwal1207 <ramannwalprajwal1@gmail.com>
2024-08-09 11:51:13 +0530

    dp check 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="gradient-bg">
       <div className="gradient-container">
-        <h1>qrio.........</h1>
+        <h1 style={{ fontSize: "20px", color: "white" }}>qrio.........</h1>
       </div>
     </div>
   );
