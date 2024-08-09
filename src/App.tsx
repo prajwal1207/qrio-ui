5e883dd386b73384c9d73fdb47ce14b4a4bb0f0a
prajwal1207 <ramannwalprajwal1@gmail.com>
2024-08-09 11:21:07 +0530

    deployment check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
     <div className="gradient-bg">
-      <div className="gradient-container"></div>
+      <div className="gradient-container">
+        <h1>qrio.........</h1>
+      </div>
     </div>
   );
 }
